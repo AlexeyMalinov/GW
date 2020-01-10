b241006f9ce4bf66fb1daf0f7c6d86db04c39500
Alexey Malinov <alexeymalinov@yandex.ru>
2020-01-11 01:36:11 +0300

    Update create-task-util

--- create-task-util/src/main/java/ru/alexeymalinov/taskautomation/Launcher.java
+++ create-task-util/src/main/java/ru/alexeymalinov/taskautomation/Launcher.java
@@ -1,76 +1,8 @@
 package ru.alexeymalinov.taskautomation;
 
-import ru.alexeymalinov.taskautomation.core.model.Task;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Launcher {
-
     public static void main(String[] args) {
-
-    }
-
-    private Task createTask(){
-        TaskMasterFactory masterFactory = TaskMasterFactory.getInstance(initializeMasters());
-        TaskMaster master = getMaster(masterFactory);
-        setOperation(master);
-        setValue(master);
-
-        return master.createTask();
-    }
-
-    private static List<TaskMaster> initializeMasters() {
-        List<TaskMaster> masters = new ArrayList<TaskMaster>();
-        masters.add(new CliTaskMaster());
-        masters.add(new GuiTaskMaster());
-        return masters;
-    }
-
-    private static TaskMaster getMaster(TaskMasterFactory masterFactory) {
-        System.out.println("Please select a task");
-        System.out.println("You can create the following tasks:");
-        masterFactory.printTypeTask();
-        TaskMaster master = null;
-        while (master == null) {
-            master = masterFactory.getInstance(new Scanner(System.in).nextLine());
-            if (master == null) {
-                System.out.println("Task type not found");
-                System.out.println("please repeat");
-            }
-        }
-        return master;
-    }
-
-    private static TaskMaster setOperation(TaskMaster master) {
-        System.out.println("Please select a operation");
-        System.out.println("You can create the following operation:");
-        master.printOperations();
-        boolean operationIsSet = false;
-        while (operationIsSet) {
-            operationIsSet = master.setOperationName(new Scanner(System.in).nextLine());
-            System.out.println("Operation not found");
-            System.out.println("please repeat");
-        }
-        return master;
-    }
-
-    private static TaskMaster setValue(TaskMaster master){
-        System.out.println("Set argument for operation");
-        master.setValue(new Scanner(System.in).nextLine());
-        return master;
-    }
-
-    private static TaskMaster setName(TaskMaster master){
-        System.out.println("Set task name");
-        master.setName(new Scanner(System.in).nextLine());
-        return master;
-    }
-
-    private static TaskMaster setServerLabel(TaskMaster master){
-        System.out.println("Set server label");
-        master.setServerLabel(new Scanner(System.in).nextLine());
-        return master;
+        TaskMaster master = new TaskMaster();
+        master.createTask();
     }
 }
--- create-task-util/src/main/java/ru/alexeymalinov/taskautomation/TaskMaster.java
+++ create-task-util/src/main/java/ru/alexeymalinov/taskautomation/TaskMaster.java
@@ -1,87 +1,126 @@
 package ru.alexeymalinov.taskautomation;
 
 import ru.alexeymalinov.taskautomation.core.model.Task;
+import ru.alexeymalinov.taskautomation.core.model.TaskBuilder;
+import ru.alexeymalinov.taskautomation.core.services.Operation;
+import ru.alexeymalinov.taskautomation.core.services.RobotService;
+import ru.alexeymalinov.taskautomation.core.services.clirobotservice.CliScriptService;
+import ru.alexeymalinov.taskautomation.core.services.guirobotservice.GuiScriptService;
 
-public abstract class TaskMaster {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    private final String serviceName;
-    private String name;
-    private String value;
-    private Task next;
-    private String serverLabel;
-    protected String operationName;
+public class TaskMaster {
 
-    TaskMaster(String serviceName) {
-        this.serviceName = serviceName;
+    private List<RobotService> services = new ArrayList<>();
+
+    private void initializeService() {
+        services.add(new CliScriptService());
+        services.add(new GuiScriptService());
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public Task getNext() {
-        return next;
-    }
-
-    public void setNext(Task next) {
-        this.next = next;
-    }
-
-    public String getOperationName() {
-        return operationName;
-    }
-
-    public String getServerName() {
-        return serverLabel;
-    }
-
-    public void setServerLabel(String serverLabel) {
-        this.serverLabel = serverLabel;
-    }
-
-
-    /**
-     * Создает задачу
-     *
-     * @return
-     */
     public Task createTask() {
-        return new Task(next, name, serverLabel, serviceName, value);
+        initializeService();
+        TaskBuilder builder = new TaskBuilder(getName());
+        RobotService service = getService();
+        return builder.serviceName(service.getClass().getName())
+                .operationName(getOperation(service))
+                .value(getValue())
+                .serverLabel(getServerLabel())
+                .next(getNext())
+                .create();
     }
 
-    /**
-     * Возвращает тип задачи
-     *
-     * @return
-     */
-    public abstract String getTaskType();
+    private RobotService getService() {
+        System.out.println("Please select a task");
+        System.out.println("You can create the following tasks:");
+        for (RobotService service : services) {
+            System.out.println(service.getClass().getSimpleName());
+        }
+        RobotService service = null;
+        while (service == null) {
+            String serviceName = new Scanner(System.in).nextLine();
+            service = serviceOfName(serviceName);
+            if (service != null) {
+                return service;
+            }
+            System.out.println("Task type not found");
+            System.out.println("please repeat");
+        }
+        return null;
+    }
 
-    /**
-     * Распечатывает в консоль все доступные операции
-     */
-    public abstract void printOperations();
+    private RobotService serviceOfName(String name) {
+        for (RobotService service : services) {
+            if (service.getClass().getSimpleName().equals(name)) {
+                return service;
+            }
+        }
+        return null;
+    }
 
-    /**
-     * Задает знаяение операции
-     * @param name
-     * @return
-     */
-    public abstract boolean setOperationName(String name);
+    private String getOperation(RobotService service) {
+        System.out.println("Please select a operation");
+        System.out.println("You can create the following operation:");
+        printOperations(service);
+        boolean operationIsCorrect = false;
+        while (!operationIsCorrect) {
+            String operationName = new Scanner(System.in).nextLine();
+            operationIsCorrect = checkOperationName(operationName, service);
+            if (operationIsCorrect) {
+                return operationName;
+            }
+            System.out.println("Operation not found");
+            System.out.println("please repeat");
+        }
+        return "";
+    }
+
+    private void printOperations(RobotService service) {
+        for (Operation operation : service.getOperations()) {
+            System.out.println(operation.getName());
+        }
+    }
+
+    private boolean checkOperationName(String operationName, RobotService service) {
+        for (Operation operation : service.getOperations()) {
+            if (operation.getName().equals(operationName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getValue() {
+        System.out.println("Set argument for operation");
+        return new Scanner(System.in).nextLine();
+    }
+
+    private String getName() {
+        System.out.println("Set task name");
+        return new Scanner(System.in).nextLine();
+    }
+
+    private String getServerLabel() {
+        System.out.println("Set server label");
+        return new Scanner(System.in).nextLine();
+    }
+
+    private Task getNext() {
+        System.out.println("Would you like to add the following task?(Yes/No)");
+        String answer = "";
+        while (answer.isEmpty()) {
+            answer = new Scanner(System.in).nextLine();
+            if ("Yes".equals(answer)) {
+                break;
+            } else if ("No".equals(answer)) {
+                return null;
+            }
+            System.out.println("please repeat");
+        }
+        TaskMaster master = new TaskMaster();
+        return master.createTask();
+    }
 
 }
