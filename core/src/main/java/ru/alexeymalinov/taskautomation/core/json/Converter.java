acb76d535331a0ac83acc6f086d93ceaacd2e30c
Alexey Malinov <alexeymalinov@yandex.ru>
2020-01-11 14:12:00 +0300

    Update converter

--- core/src/main/java/ru/alexeymalinov/taskautomation/core/json/Converter.java
+++ core/src/main/java/ru/alexeymalinov/taskautomation/core/json/Converter.java
@@ -3,15 +3,28 @@ package ru.alexeymalinov.taskautomation.core.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Converter<T> {
 
-    public String toJSON (T t) throws JsonProcessingException {
+    public String toJSON(T t) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(t);
+    }
+
+    public void toJSON(File file, T t) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(file, t);
     }
 
     public T toObject(String content, Class<T> cls) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(content, cls);
+    }
+
+    public T toObject(File file, Class<T> cls) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(file, cls);
     }
 }
